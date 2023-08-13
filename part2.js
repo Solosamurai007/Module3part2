249a5fb67297e4671134f95353dd75c46a54832d
hirokokukla <hiroko.kukla.cohort233@gmail.com>
2023-08-13 15:41:16 -0700

    first commit

---
+++ part2.js
@@ -0,0 +1,33 @@
+// Function to fetch weather data using the fetch() API
+async function fetchWeather(city) {
+    const apiKey = "YOUR_API_KEY"; // Replace with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        throw new Error("Unable to fetch weather data");
+    }
+}
+
+// Event handler for the "Get Weather" button
+document.getElementById("getWeatherBtn").addEventListener("click", async () => {
+    const city = document.getElementById("cityInput").value;
+    const weatherInfo = document.getElementById("weatherInfo");
+
+    weatherInfo.textContent = "Fetching weather data...";
+
+    try {
+        const weatherData = await fetchWeather(city);
+        weatherInfo.innerHTML = `
+            <p>City: ${weatherData.name}</p>
+            <p>Temperature: ${weatherData.main.temp}°C</p>
+            <p>Description: ${weatherData.weather[0].description}</p>
+        `;
+    } catch (error) {
+        weatherInfo.textContent = "Error fetching weather data";
+        console.error(error);
+    }
+});
